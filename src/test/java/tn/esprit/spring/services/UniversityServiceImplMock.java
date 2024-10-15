0804a7ecd457900a0b59e6fa6ce3a40125a03bdb
bouazizisalim <salim.bouazizi@esprit.tn>
2024-10-15 12:44:27 +0100

    test mock finished

--- src/test/java/tn/esprit/spring/services/UniversityServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UniversityServiceImplMock.java
@@ -1,5 +1,4 @@
 package tn.esprit.spring.services;
-
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.tpfoyer.entity.Universite;
 import tn.esprit.tpfoyer.repository.UniversiteRepository;
 import tn.esprit.tpfoyer.service.UniversiteServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 //@TestMethodOrder(Cla)
-@ExtendWith(MockitoExtension.class)
-class UniversityServiceImplMock {
+class TestUniversityServiceImplMock {
     @Mock
     private UniversiteRepository universiteRepository;
     @InjectMocks
@@ -36,7 +32,6 @@ class UniversityServiceImplMock {
             .build();
 
 
-    Universite u = new Universite(1, "ENSI", "Tunis", null);
     List<Universite> listUniversites = new ArrayList<Universite>() {
         {
             add(new Universite(2, "ENSI", "Tunis", null));
@@ -54,21 +49,54 @@ class UniversityServiceImplMock {
         Mockito.verify(universiteRepository, Mockito.times(1)).findAll();
     }
     // Test for retrieving a single university by id
-  /* @Test
-    @Order(2)
-     void testRetrieveUniversite() {
-        Mockito.when(universiteRepository.findById(u.getIdUniversite())).thenReturn(Optional.of(u));
-        Universite result = universiteService.retrieveUniversite(u.getIdUniversite());
-        assertEquals(u, result);
-        Mockito.verify(universiteRepository, Mockito.times(4)).findById(u.getIdUniversite());
+   @Test
+   @Order(2)
+   void testRetrieveUniversite() {
+       Mockito.when(universiteRepository.findById(universite3.getIdUniversite())).thenReturn(Optional.of(universite3));
+       Universite result = universiteService.retrieveUniversite(universite3.getIdUniversite());
+       assertEquals(universite3, result);
+       Mockito.verify(universiteRepository, Mockito.times(1)).findById(universite3.getIdUniversite());
+   }
+
+    @Test
+    @Order(3)
+    void testmodifyUniversity() {
+        Universite universityToUpdate = listUniversites.get(0); // Assume we modify the first student
+        universityToUpdate.setAdresse("UpdatedAddress");
+
+        Mockito.when(universiteRepository.save(universityToUpdate)).thenReturn(universityToUpdate);
+
+        Universite updatedUniversity = universiteService.modifyUniversite(universityToUpdate);
+
+        assertNotNull(updatedUniversity);
+        assertEquals("UpdatedAddress", updatedUniversity.getAdresse());
+        Mockito.verify(universiteRepository, Mockito.times(1)).save(universityToUpdate);
     }
     // Test for removing a university
     @Test
     @Order(4)
-     void testRemoveUniversite() {
-        Long universiteId = 1L;
-        Mockito.doNothing().when(universiteRepository).deleteById(universiteId);
-        universiteService.removeUniversite(universiteId);
-        Mockito.verify(universiteRepository, Mockito.times(4)).deleteById(universiteId);
-    }*/
+    void testRemoveNonExistentUniversite() {
+        Long universityIdToRemove = listUniversites.get(0).getIdUniversite();
+        universiteService.removeUniversite(universityIdToRemove);
+        Mockito.verify(universiteRepository, Mockito.times(1)).deleteById(universityIdToRemove);
+    }
+    @Test
+    @Order(5)
+    void testAddUniveristy() {
+        Universite newUniveristy = Universite.builder()
+                .idUniversite(4L)
+                .foyer(null)
+                .nomUniversite("ESB")
+                .adresse("ARiana")
+                .build();
+        Mockito.when(universiteRepository.save(newUniveristy)).thenReturn(newUniveristy);
+
+        Universite addedUniveristy = universiteService.addUniversite(newUniveristy);
+
+        assertNotNull(addedUniveristy);
+        assertEquals(newUniveristy.getNomUniversite(), addedUniveristy.getNomUniversite());
+        Mockito.verify(universiteRepository, Mockito.times(1)).save(newUniveristy);
+
+
+    }
 }
